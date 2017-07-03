943f0173cea6ac33b79f13ca72a229cd0a05a0d5
alexa-coding <alexasoucy123@gmail.com>
2017-07-03 10:33:59 -0700

    Added [PARAMETER]

--- 6-javascript_continued/0-parameter.js
+++ 6-javascript_continued/0-parameter.js
@@ -1,4 +1,3 @@
 let my_parameter = process.argv[2];
 
-console.log("Here is my first passed parameter:" + my_parameter)
-test
+console.log("Here is my first passed parameter: [PARAMETER]" + my_parameter)
