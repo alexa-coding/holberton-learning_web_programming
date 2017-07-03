652a5fc31d4c326f7d40b408c860788031f91d15
alexa-coding <alexasoucy123@gmail.com>
2017-07-03 10:59:59 -0700

    Using else statement

---
+++ 6-javascript_continued/2-what_else.js
@@ -0,0 +1,10 @@
+let my_parameter = process.argv[2];
+let a = 5
+let b = my_parameter
+
+if (a < b) {
+  console.log("The value is greater than 5")
+}
+else {
+  console.log("The value is equal to 5 or lower")
+}
